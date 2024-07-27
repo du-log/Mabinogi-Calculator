2b5504afd3d8ccf6a601eccc148cbe64b89a5935
Brandon Dulog <vexoruz@gmail.com>
2024-07-27 09:07:04 -0400

    Modified MainGrid, added another TabControl, fixed Bindings

--- Mabinogi_Calculator_WPF/MVVM/Model/Calculations.cs
+++ Mabinogi_Calculator_WPF/MVVM/Model/Calculations.cs
@@ -11,69 +11,59 @@ namespace Mabinogi_Calculator_WPF.MVVM.Model
     {
         public Calculations() { }
 
+        private double CalculateStat(int baseStat, double divisor)
+        {
+            return ((baseStat - 10) / divisor);
+        }
+
         public int CalcBaseDefense(int strength)
         {
             //Calculates base defense from total strength stat (base + modifiers, yellow value in-game), first 10 strength are non-existent.
-            double baseDef = (strength - 10) / 10;
-            return (int)baseDef;
+            return (int)CalculateStat(strength, 10);
         }
         public int CalcBaseMagicDefense(int will)
         {
             //Calculates base magic defense from total will stat (base + modifiers, yellow value in-game), first 10 will are non-existent.
-            double baseMDef = (will - 10) / 10;
-            return (int)baseMDef;
+            return (int)CalculateStat(will, 10);
         }
         public int CalcBaseMagicProtection(int intelligence)
         {
             //Calculates base magic protection from total intelligence stat (base + modifiers, yellow value in-game), first 10 intelligence are non-existent.
-            double baseMProt = (intelligence - 10) / 20;
-            return (int)baseMProt;
+            return (int)CalculateStat(intelligence, 20);
         }
         public int CalcBaseMagicAttack(int intelligence)
         {
-            double baseMA = (intelligence - 10) / 5;
-            return (int)baseMA;
+            return (int)CalculateStat(intelligence, 5);
         }
-        public int CalcMinimumDamage(int strength) 
+        public int CalcMinimumDamage(int strength)
         {
             //Calculate minimum damage from total strength stat
-            double minDmg = (strength - 10) / 3;
-            return (int)minDmg;
+            return (int)CalculateStat(strength, 3);
         }
         //public int CalcMinimumDamage(int strength, string weapon){} Placeholder for when weapons implemented
-        public int CalcMaximumDamage(int strength) 
+        public int CalcMaximumDamage(int strength)
         {
-            double maxDmg = (strength - 10) / 2.5;
-            return (int)maxDmg;
+            //Calculate maximum damage from total strength stat
+            return (int)CalculateStat(strength, 2.5);
         }
         public double CalcCriticalRate(int will, int luck)
         {
-            double critRate = (((double)will - 10) / 10) + (((double)luck - 10) / 5);
-            critRate = Math.Round(critRate, 1, MidpointRounding.ToZero);
-            return critRate;
+            double critRate = (((double)will - 10) / 10.0) + (((double)luck - 10) / 5.0);
+            return Math.Round(critRate, 1, MidpointRounding.ToZero);
         }
         public int CalcMaximumInjury(int dex, int will)
         {
             double maxInjury = ((dex - 10) / 10) + (2 * ((will - 10) / 10));
-            if (maxInjury >= 100.0)
-            {
-                maxInjury = 100.0;
-            }
-            return (int)maxInjury;
+            return (int)Math.Min(maxInjury, 100.0);
         }
         public int CalcMinimumInjury(int dex, int will)
         {
             double minInjury = (0.5 * ((dex - 10) / 10)) + (0.5 * ((will - 10) / 10));
-            if(minInjury >= 100.0)
-            {
-                minInjury = 100.0;
-            }
-            return (int)minInjury;
+            return (int)Math.Min(minInjury, 100.0);
         }
         public int CalcArmorPierce(int dex)
         {
-            double armorPierce = ((double)dex - 10)/ 15;
-            return (int)armorPierce;
+            return (int)CalculateStat(dex, 15);
         }
     }
 }
